f45c99b6bbd040be03c08aef70eb7b1e16005794
jhson <jhson@encar.com>
2017-02-23 11:37:46 +0900

    db경로 수정

--- server/src/main.js
+++ server/src/main.js
@@ -18,8 +18,8 @@ db.on('error', console.error);
 db.once('open', () => {
     console.log('Connected to mongodb server');
 });
-//mongoose.connect('mongodb://jhson:wjdgh0754522@ds157549.mlab.com:57549/serv_urls');
-mongoose.connect('mongodb://localhost/serv_urls');
+mongoose.connect('mongodb://jhson:wjdgh0754522@ds157549.mlab.com:57549/serv_urls');
+//mongoose.connect('mongodb://localhost/serv_urls');
 
 app.use('/', express.static(__dirname + '/../../public'));
 app.use('/api', api)
